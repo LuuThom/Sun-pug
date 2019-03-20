b5083fa4d58170bc53c3400433279e1263f0f58c
LuuThom <thom0702>
2019-03-20 09:32:36 +0700

    new-commit-20/03

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-
+$(document).ready(function(){
+    (function($) {
+        "use strict";
+        var $header = $(".header"),
+            y_pos = $header.offset().top,
+            height = $header.height();
+    
+        $(window).scroll(function() {
+            var scrollTop = $(this).scrollTop();
+            if (scrollTop > 0) {
+                $header.addClass("header-fix");
+            } else {
+                $header.removeClass("header-fix");  
+            }
+        });
+    
+    })(jQuery, undefined);
+    
+});
